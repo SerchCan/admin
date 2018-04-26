6f039ef9025591b14689c316b86786246ea00c37
Sergio Can <gersiopumas@gmail.com>
2018-04-26 15:45:58 -0500

    Client(id) retrieve user data (still on update)

--- admin/Client.php
+++ admin/Client.php
@@ -1,51 +1,37 @@
 <?php
     include_once "User.php";
     // Client can only get his own data but cannot set it.
-    
     class Client extends User{
-	  private $accountNumber;
-      private $cardNumber;
-      public function __construct(){
-          $con = new PDORepository;  
-          //Example use: QUERY, ARGS -> fetchAll or fetch single record.  you decide
-          //echo $con->queryList("SELECT * FROM ventas",null)->fetchAll());
-          //$con->queryList("SELECT * FROM Usuarios where  ")
-
-      }
-	  public function getName(){
-          return $this->name;
-      }
-      public function getAccountNumber(){
-          return $this->AccountNumber;
-      }
-      public function getCard(){
-          return $this->CardNumber;
-      }
-      public function getUser(){
-          return $this->user;
-      }
-	  public function getMail(){
-	      return $this->email;
-      }  
-      public function getStatus(){
-          return $this->status;
-      }
-      public function getAddress(){
-          return $this->address;
-      }
-      public function getGenre(){
-          return $this->genre;
-      }
-      private function getPassword(){
-          return $this->password;
-      }
-      public function getRFC(){
-          return  $this->RFC;
-      }
-      public function getCURP(){
-          return  $this->CURP;
-      }
+	    private $accountNumber;
+        private $cardNumber;
+        public function __construct($id){
+            $con = new PDORepository;  
+            //Example use: $con->(QUERY, ARGS)-> 'fetchAll()' or 'fetch()' single record.  you decide
+            $userData = $con-> queryList("SELECT * FROM Usuario where id_usuario=:id", array("id"=>$id))
+                            -> fetch(PDO::FETCH_ASSOC);
+            if($userData){
+                $id=$userData['id_usuario'];
+                $name=$userData['nombre'];
+                $email=$userData['correo'];
+                $user=$userData['username'];
+                $password=$userData['password'];
+                $status=$userData['estatus'];
+                $alta=$userData['fecha_alta'];
+                $address=$userData['direccion'];
+                $genre=$userData['sexo'];
+                $rfc=$userData['rfc'];
+                $curp=$userData['curp'];
+                $this->setUser($id,$name,$email,$user,$password,$status,$alta,$address,$genre,$rfc,$curp);
+            }
+            else{
+                echo "No se encontro el id de usuario en el sistema.";
+            }
+        }
+        public function getAccountNumber(){
+            return $this->AccountNumber;
+        }
+        public function getCard(){
+            return $this->CardNumber;
+        }
     }
-
-    $client= new Client();
 ?>
