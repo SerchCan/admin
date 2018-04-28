e7ad19f4f14e3ef076ee62bef11e9db97ef5a45f
Sergio Can <gersiopumas@gmail.com>
2018-04-28 16:00:17 -0500

    Fill user data with id

--- admin/Client.php
+++ admin/Client.php
@@ -4,16 +4,6 @@
     class Client extends User{
 	    private $accountNumber;
         private $cardNumber;
-        
-        public function createUser($name,$mail,$user,$password,$address,$genre,$rfc,$curp,$idPais){
-            $con = new PDORepository;
-            $result = $con-> queryList("INSERT INTO Usuario(nombre,correo,username,password,estatus,fecha_alta,direccion,sexo,rfc,curp) 
-                                        VALUES(:nombre, :email, :user, :pass, ACTIVO, CURRENT_TIMESTAMP(), :address, :genre, :rfc, :curp, :pais)",
-                                        array('nombre'=>$name, 'email'=>$mail, 'user'=>$user, 'pass'=>$password, 'address'=>$address, 'genre'=>$genre, 'rfc'=>$rfc, 'curp'=>$curp, 'pais'=>$idPais));
-            if(!$result){
-                echo "Ha ocurrido un error al crear usuario.";
-            }
-        }
         public function fillUser($id){
             $con = new PDORepository;  
             $userData = $con-> queryList("SELECT * FROM Usuario WHERE id_usuario=:id", array('id'=>$id))
@@ -30,17 +20,28 @@
                 $genre = $userData['sexo'];
                 $rfc = $userData['rfc'];
                 $curp = $userData['curp'];
-                $this-> setUser($id,$name,$email,$user,$password,$status,$alta,$address,$genre,$rfc,$curp);
+                $this->setUser($id,$name,$email,$user,$password,$status,$alta,$address,$genre,$rfc,$curp);
+                $this->setClient();
             }
             else{
                 echo "No se encontro el id de usuario en el sistema.";
             }
         }
+        public function setClient(){
+            $con = new PDORepository;
+            $id = $this->getId();
+            $data = $con->queryList("SELECT cuenta.numero AS accountNumber, tarjeta.numero AS cardNumber FROM cuenta INNER JOIN tarjeta
+                                    WHERE tarjeta.id_cuenta = cuenta.id_cuenta AND cuenta.id_usuario = :id",
+                                    array('id'=>$id))
+                        -> fetch(PDO::FETCH_ASSOC);
+            $this->accountNumber=$data['accountNumber'];
+            $this->cardNumber=$data['cardNumber'];
+        }
         public function getAccountNumber(){
-            return $this->AccountNumber;
+            return $this->accountNumber;
         }
         public function getCard(){
-            return $this->CardNumber;
+            return $this->cardNumber;
         }
     }
 ?>
