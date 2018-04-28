d21e49a0f155c4bba2fbd69fdb68e96764af480a
Sergio Can <gersiopumas@gmail.com>
2018-04-28 15:59:38 -0500

    Added create user function

--- admin/Admin.php
+++ admin/Admin.php
@@ -1,5 +1,14 @@
 <?php
     class Admin extends User{
-        
+         
+        public function createUser($name,$mail,$user,$password,$address,$genre,$rfc,$curp,$userAlta,$idPais){
+            $con = new PDORepository;
+            $result = $con-> queryList("INSERT INTO Usuario(nombre, correo, username, password, estatus, tipo, fecha_alta, direccion, sexo, rfc, curp, usuario_alta, id_pais) 
+            VALUES(:nombre, :email, :user, :pass, ACTIVO, CLIENTE, CURRENT_TIMESTAMP(), :address, :genre, :rfc, :curp, :userAlta :pais)",
+            array('nombre'=>$name, 'email'=>$mail, 'user'=>$user, 'pass'=>$password, 'address'=>$address, 'genre'=>$genre, 'rfc'=>$rfc, 'curp'=>$curp, 'userAlta'=>$userAlta,'pais'=>$idPais));
+            if(!$result){
+                echo "Ha ocurrido un error al crear usuario.";
+            }
+        }
     }
 ?>
