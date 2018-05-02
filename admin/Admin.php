9ec9f11e1aac05dcd8c4813300a86ee86beacb93
Sergio Can <gersiopumas@gmail.com>
2018-05-02 10:48:55 -0500

    Permissions for operations

--- admin/Admin.php
+++ admin/Admin.php
@@ -1,32 +1,60 @@
 <?php
     include_once "User.php";
     class Admin extends User{
+        private $canOperate=true;
+        public function __construct($user,$pass){
+            $res=$this->Login($user,$pass);
+            if(!$res || $res['tipo']!="ADMINISTRADOR"){
+                echo "No tiene permiso de administrador";
+                $this->canOperate=false;
+            }
+            return;
+        }
+        
         public function createUser($name, $mail, $user, $password, $address, $genre, $rfc, $curp, $userAlta, $idPais, $balance, $detalles, $tipo){
-            $con = new PDORepository;
-            
-            $Createuser = $con-> queryList('INSERT INTO usuario(nombre, correo, username, password, estatus, tipo, fecha_alta, direccion, sexo, rfc, curp, usuario_alta, id_pais) 
-            VALUES(:nombre, :email, :user, :pass, "ACTIVO", "CLIENTE", CURRENT_TIMESTAMP(), :address, :genre, :rfc, :curp, :userAlta, :pais)',
-            array('nombre'=>$name, 'email'=>$mail, 'user'=>$user, 'pass'=>$password, 'address'=>$address, 'genre'=>$genre, 'rfc'=>$rfc, 'curp'=>$curp, 'userAlta'=>$userAlta,'pais'=>$idPais));
-            
-            $id = $this->getLastInsert();
-            $accountNumber = $this->generateAccount($password,$curp,$idPais);
-
-            $CreateAccount=$con->queryList('INSERT INTO cuenta(numero,balance,detalles,fecha_alta,estatus,tipo,id_usuario)
-                VALUES (:acnumber,:balance,:details, CURRENT_TIMESTAMP(), "ACTIVA", :tipo, :id)',
-                array('acnumber'=>$accountNumber,'balance'=>$balance,'details'=>$detalles,'tipo'=>$tipo,'id'=>$id));
-            
+            if($this->canOperate==true)
+            {  
+                $con = new PDORepository;
+                $tipo=strtoupper($tipo);
+                $Createuser = $con-> queryList('INSERT INTO usuario(nombre, correo, username, password, estatus, tipo, fecha_alta, direccion, sexo, rfc, curp, usuario_alta, id_pais) 
+                VALUES(:nombre, :email, :user, :pass, "ACTIVO", :tipo, CURRENT_TIMESTAMP(), :address, :genre, :rfc, :curp, :userAlta, :pais)',
+                array('nombre'=>$name, 'email'=>$mail, 'user'=>$user, 'pass'=>$password, 'tipo'=>$tipo, 'address'=>$address, 'genre'=>$genre, 'rfc'=>$rfc, 'curp'=>$curp, 'userAlta'=>$userAlta,'pais'=>$idPais));
+                
+                if($tipo=="CLIENTE"){
+                    $id = $this->getLastInsert();
+                    $accountNumber = $this->generateAccount($password,$curp,$idPais);
+    
+                    $CreateAccount=$con->queryList('INSERT INTO cuenta(numero,balance,detalles,fecha_alta,estatus,tipo,id_usuario)
+                        VALUES (:acnumber,:balance,:details, CURRENT_TIMESTAMP(), "ACTIVA", :tipo, :id)',
+                        array('acnumber'=>$accountNumber,'balance'=>$balance,'details'=>$detalles,'tipo'=>$tipo,'id'=>$id));       
+                }
+            }
+            else{
+                echo "No tiene permisos para esta operación";
+            }
         }
 
         public function getLastInsert(){
-            $con = new PDORepository;
-            $query= $con-> queryList("SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1",null)
-                        -> fetch(PDO::FETCH_ASSOC);
-            return $query['id_usuario'];
+            if($this->canOperate){
+                $con = new PDORepository;
+                $query= $con-> queryList("SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1",null)
+                            -> fetch(PDO::FETCH_ASSOC);
+                return $query['id_usuario'];
+            }
+            else{
+                return "No tiene permisos para esta operación";
+            }
         }
         public function generateAccount($password,$curp,$idPais){
-            //                  country           EP(EdisonPay) CURP      PASSWORD
-            $accountNumber = sprintf("%02d", $idPais)."6980".ord($curp).ord($password);
-            return $accountNumber;
+            if($this->canOperate){
+                //                  country           EP(EdisonPay) CURP      PASSWORD
+                $accountNumber = sprintf("%02d", $idPais)."6980".ord($curp).ord($password);
+                return $accountNumber;
+            }
+            else{
+                echo "No tiene permisos para esta operación";
+            }
         }
     }
+    $u=new Admin("sergiopumas","Password");
 ?>
