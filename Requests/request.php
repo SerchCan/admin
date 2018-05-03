d6718d8a0f2b53d7d9007b6a86568e7eb98a6a32
Sergio Can <gersiopumas@gmail.com>
2018-05-02 23:18:41 -0500

    Request example

---
+++ Requests/request.php
@@ -0,0 +1,24 @@
+<?php    
+    /**
+     * How to program a request.php
+     * if (js request POST({user:user,password:pass});
+     * you get the data from POST
+     */                 
+    $_POST['user'];
+    $_POST['pass'];
+    /**
+     * if (js request GET({id:1}))
+     * you get data from GET
+     */
+    $_GET['id'];
+
+    // to check if the variable exist you use isset() function
+    isset($_POST['user']);
+    isset($_GET['id']);
+    // note: you can put as many variables as you want to isset
+    isset($_POST['user'],$_GET['id']);
+
+    //POST and GET variables are differents between them
+    // $_POST['id'] != $_GET['id']; they are in a different context type.
+
+?>
