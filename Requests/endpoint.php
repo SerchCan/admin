50921542db58a9c47cc927ce0b941f6704c6a3d5
Sergio Can <gersiopumas@gmail.com>
2018-05-10 21:46:42 -0500

    endpoint update

--- Requests/endpoint.php
+++ Requests/endpoint.php
@@ -14,17 +14,25 @@ include_once '../Includes/Include.php';
 if(isset($_POST['usuario'],$_POST['password'])){
     $e=new Executive($_POST['usuario'],$_POST['password']);
     if($e->isLogged){
-        if(isset($_POST['id_usuario'],$_POST['pin'])){
+        if(isset($_POST['id_usuario'],$_POST['pin'],$_POST['tipo_cuenta'],$_POST['detalles'])){
+            $con = new PDORepository;
+            $id=$_POST['id_usuario'];
             $pin=$_POST['pin'];
+            $tipoCuenta=strtoupper($_POST['tipo_cuenta']);
+            $detalles=$_POST['detalles'];
             $account=generateAccount($_POST['id_usuario']);
+            $CreateAccount=$con->queryList('INSERT INTO cuenta(numero,balance,detalles,fecha_alta,estatus,tipo,id_usuario)
+                            VALUES (:acnumber,0,:details, CURRENT_TIMESTAMP(), "ACTIVA", :tipo, :id)',
+                            array('acnumber'=>$account,'details'=>$detalles,'tipo'=>$tipoCuenta,'id'=>$id));       
             $Card= new CreditCard;
             $Card->generate($pin);
-        
+            $Card->addToUser($account);
+
             $all= array('cuenta'=>$account,'tarjeta'=>$Card->getCard());
             echo json_encode($all);
         }
         else{
-            echo "Falta el id o pin de usuario";
+            echo "Falta el id, pin, tipo de cuenta o detalle del usuario";
         }
     }
     
